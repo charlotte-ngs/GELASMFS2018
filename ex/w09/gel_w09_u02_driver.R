c7cc59006257dfee7e863bdc5a150f5eefdf7df8
charlotte-ngs <ngs.charlotte@gmail.com>
2018-04-20 09:06:20 +0200

    Adding sol and ex for w09

---
+++ ex/w09/gel_w09_u02_driver.R
@@ -0,0 +1,24 @@
+###
+###
+###
+###   Purpose:   Prototype of a driver script for a course document 
+###   started:   2018-04-16 (pvr)
+###
+### ################################################################ ###
+
+### # directory constants
+### # rstudio project directory
+s_rstudio_proj_dir <- rprojroot::find_rstudio_root_file()
+### # full path to this file
+s_this_file <- rprojroot::thisfile()
+### # document source directory
+s_doc_src_dir <- dirname(s_this_file)
+### # basename of this file
+s_base_this_file <- basename(s_this_file)
+### # replace "_driver.R" in s_base_this_file by .Rmd to get to Rmd-src file
+s_rmd_src <- gsub(pattern = "_driver.R", replacement = ".Rmd", x = s_base_this_file, fixed = TRUE)
+
+### # put together full path to rmd-src file and call deployment
+s_rmd_src_fp <- file.path(s_doc_src_dir, s_rmd_src)
+teachtools::deploy_to_gh_pages(ps_doc_src_file = s_rmd_src_fp)
+
